552b2b7ac1cd985fb9b0c12bbb01dcfb9fc9abf9
shmulim <shmuel.markel@gmail.com>
2015-06-03 18:33:21 -0700

    added model/collection instances

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
-var blog = Backbone.Model.extend({
+// Backbone Model
+var Blog = Backbone.Model.extend({
 	defaults: {
 		'name': '',
 		'title': '',
 		'url': ''
 	}
 });
+
+// Backbone Collection
+var Blogs = Backbone.Collection.extend({});
+
+// Create 2 Blog model instances
+var blog1 = new Blog({
+	'name': 'shmuli',
+	'title': 'title here',
+	'url': 'url here'
+});
+
+var blog2 = new Blog({
+	'name': 'shmulim',
+	'title': 'second title',
+	'url': 'second url here'
+});
+
+// Create a Blogs collection instance
+var blogs = new Blogs([blog1, blog2]);
